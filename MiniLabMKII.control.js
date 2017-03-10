22a78e1c755699f64513117e02047bae1014afeb
Netsu <netsu@wro-mxpawl-ml01.local>
2017-03-10 13:57:00 +0100

    code cleanup

--- MiniLabMKII.control.js
+++ MiniLabMKII.control.js
@@ -1,4 +1,3 @@
-
 loadAPI(1);
 
 host.defineController("Arturia", "MiniLab mkII - Netsu", "1.0", "6E55D132-1846-4C64-9F97-48041F2D9B96");
@@ -6,6 +5,7 @@ host.defineMidiPorts(1, 1);
 host.addDeviceNameBasedDiscoveryPair(["Arturia MINILAB MKII"], ["Arturia MINILAB MKII"]);
 
 var TRACK_NUM = 8;
+var SEND_NUM = 2;
 
 var Knobs1 = [112, 74, 71, 76, 77, 93, 73, 75];
 var Knobs2 = [114, 18, 19, 16, 17, 91, 79, 72];
@@ -29,12 +29,10 @@ function init()
     MiniLabPads.assignPolyphonicAftertouchToExpression(0, NoteExpression.TIMBRE_UP, 2);
 
     host.getMidiInPort(0).setMidiCallback(onMidi);
-    host.getMidiInPort(0).setSysexCallback(onSysex);
-
-    transport = host.createTransport();
+    //host.getMidiInPort(0).setSysexCallback(onSysex);
 
     // create track bank, groups are added without children
-    tracks = host.createMasterTrack(0).createSiblingsTrackBank(TRACK_NUM, 2, 0, false, false);
+    tracks = host.createMasterTrack(0).createSiblingsTrackBank(TRACK_NUM, SEND_NUM, 0, false, false);
     cTrack = host.createCursorTrack(3, 0);
     cDevice = cTrack.getPrimaryDevice();
 
@@ -45,15 +43,7 @@ function init()
         })
     }
 
-    // Make CCs 1-119 freely mappable
-    //userControls = host.createUserControls(HIGHEST_CC - LOWEST_CC + 1);
-
     setIndications();
-
-    //for(var i=LOWEST_CC; i<=HIGHEST_CC; i++)
-    //{
-    //    userControls.getControl(i - LOWEST_CC).setLabel("CC" + i);
-    //}
 }
 
 function MidiData(status, data1, data2)
@@ -68,12 +58,9 @@ function onMidi(status, data1, data2)
     // Instantiate the MidiData Object for convenience:
     var midi = new MidiData(status, data1, data2);
 
-    //if (midi.data1 == 0 && midi.status != 176)
-    //{
-        println(midi.status + ":" + midi.data1 + ":" + midi.data2);
-    //}
+    //println(midi.status + ":" + midi.data1 + ":" + midi.data2);
 
-    // status 176 is for knobs I think
+    //status 176 is for knobs I think
     if (midi.status == 176)
     {
         for (var i = 0; i < 8; i++)
@@ -138,27 +125,6 @@ function knobFunc(Row, index, midi)
     }
 }
 
-function onSysex(data) {
-   // MMC Transport Controls:
-   switch (data) {
-      case "f07f7f0605f7":
-         transport.rewind();
-         break;
-      case "f07f7f0604f7":
-         transport.fastForward();
-         break;
-      case "f07f7f0601f7":
-         transport.stop();
-         break;
-      case "f07f7f0602f7":
-         transport.play();
-         break;
-      case "f07f7f0606f7":
-         transport.record();
-         break;
-   }
-}
-
 function setIndications()
 {
     for (var i = 0; i < 8; i++)
@@ -167,7 +133,6 @@ function setIndications()
         cDevice.getMacro(i).getAmount().setIndication(true);
     }
 }
-
 
 function exit()
 {
